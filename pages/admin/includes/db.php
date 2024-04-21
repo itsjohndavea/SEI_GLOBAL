ed045e411f6cdf7ddd741ab8fe19f74a76b8aa74
Davee <johndaveaquino35@gmail.com>
2024-04-21 21:50:36 +0800

    Update db.php

--- pages/admin/includes/db.php
+++ pages/admin/includes/db.php
@@ -1,4 +1,4 @@
 <?php
-$DSN='mysql:host=217.21.73.201; dbname=u826177368_sei_blog';
-$ConnectingDB = new PDO($DSN,'u826177368_sei','roudhQCL123!');
+$DSN='mysql:host=localhost:3306; dbname=sei_blog';
+$ConnectingDB = new PDO($DSN,'root','');
 ?>
